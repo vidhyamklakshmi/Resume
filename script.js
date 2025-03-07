0450992d5971af8a64966203f20440778bb8d97e
vidhyamklakshmi <mkvlakshmisathya@gmail.com>
2025-03-07 13:56:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("theme-toggle").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    this.textContent = document.body.classList.contains("dark-mode") ? "☀️ Light Mode" : "🌙 Dark Mode";
+});
